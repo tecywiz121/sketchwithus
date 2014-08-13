48d1f6f2fa7523d1f3c89107c4620de2cb6b05fc
Sam Wilson <tecywiz121@hotmail.com>
2014-08-13 15:19:56 -0400

    Add license for JS

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2014 Sam Wilson <tecywiz121@gmail.com>
+ *
+ * This file is part of SketchWith.Us.
+ *
+ * SketchWith.Us is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SketchWith.Us is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with SketchWith.Us.  If not, see <http://www.gnu.org/licenses/>.
+ */
 (function(global) {
     'use strict';
 
